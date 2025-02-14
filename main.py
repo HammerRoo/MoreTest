c6ca3bb4472ba910e46fc0db61d7f7aaeab7dbac
Kirill Palchevskiy <hammerroofficial@gmail.com>
2025-02-14 18:26:48 +0300

    added dilation

--- main.py
+++ main.py
@@ -12,23 +12,27 @@ def save_image(image, name):
 
 def preprocess_image(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    save_image(gray, "1_gray.jpg")
+    save_image(gray, "1_gray.png")
 
     blurred = cv2.GaussianBlur(gray, (9, 9), 3)
-    save_image(blurred, "2_blurred.jpg")
+    save_image(blurred, "2_blurred.png")
 
     thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
                                    cv2.THRESH_BINARY_INV, 11, 2) # 11,2 стандарт, 19,3 также хороший результат
-    save_image(thresh, "3_thresh.jpg")
+    save_image(thresh, "3_thresh.png")
 
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
     opened = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
-    save_image(opened, "4_opened.jpg")
+    save_image(opened, "4_opened.png")
 
     closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, kernel, iterations=3) # на 3 видно рамкой весь номер, на 2 по отдельности цифры
-    save_image(closed, "5_closed.jpg")
+    save_image(closed, "5_closed.png")
 
-    return closed
+    dilated_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    dilated = cv2.dilate(closed, dilated_kernel, iterations=1)
+    save_image(dilated, "6_dilated.png")
+
+    return dilated
 
 def find_and_draw_digits(image, processed_image):
     contours, _ = cv2.findContours(processed_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
